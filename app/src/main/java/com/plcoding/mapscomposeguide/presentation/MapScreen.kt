4d7d0e6dd847023fbf2db974b836983842cce7c7
Ashwani1330 <ashwanihabri@gmail.com>
2023-08-20 23:47:38 +0530

    Initial commit

---
+++ app/src/main/java/com/plcoding/mapscomposeguide/presentation/MapScreen.kt
@@ -0,0 +1,64 @@
+package com.plcoding.mapscomposeguide.presentation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ToggleOff
+import androidx.compose.material.icons.filled.ToggleOn
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.MapUiSettings
+import com.google.maps.android.compose.Marker
+import com.google.maps.android.compose.rememberCameraPositionState
+
+@Composable
+fun MapScreen(
+    viewModel: MapViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
+) {
+    val scaffoldState = rememberScaffoldState()
+    val vellore = LatLng(12.9691892, 79.1559470)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(vellore, 15f)
+    }
+
+    val uiSettings = remember {
+        MapUiSettings(zoomControlsEnabled = false)
+    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        floatingActionButton = {
+            FloatingActionButton(onClick = {
+                viewModel.onEvent(MapEvent.ToggleFalloutMap)
+            }) {
+                Icon(
+                    imageVector = if (viewModel.state.isFalloutMap) {
+                        Icons.Default.ToggleOff
+                    } else Icons.Default.ToggleOn,
+                    contentDescription = "Toggle Fallout Map"
+                )
+            }
+        }
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            properties = viewModel.state.properties,
+            uiSettings = uiSettings,
+            onMapLongClick = {
+            },
+            cameraPositionState = cameraPositionState
+        ) {
+            Marker(
+                position = vellore,
+                title = "VIT Vellore",
+                snippet = "Marker in Vellore"
+            )
+        }
+    }
+}
